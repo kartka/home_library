09526baad05a3896d778d6ff5e4df7a4d3580c92
kartka <a.wojdon@gmail.com>
2014-12-07 14:27:24 +0100

    Update NewBook.java

--- Book/NewBook.java
+++ Book/NewBook.java
@@ -1,27 +1,47 @@
 import java.util.Scanner;
 
 public class Book {
+	
+	String title;
+	String authorsName;
+	String authorsSurname;
+	String publishingHouse;
+	String literaryGenre; 
+	int dateOfPublication; 
+	
+	public Book(String t, String aN, String aS, String pH, String lG, int dOP) {
+		
+		title = t;
+		authorsName = aN;
+		authorsSurname = aS;
+		publishingHouse= pH;
+		literaryGenre= lG; 
+		dateOfPublication= dOP; 
+		
+	}
+	
 	public static void main(String[] args){
-		Scanner book = new Scanner(System.in);
+		Scanner take = new Scanner(System.in);
 		
 		System.out.print("Title: ");
-		String title= book.nextLine();
+		String title= taje.nextLine();
 
 		System.out.print("Author's name: ");
-		String authorsName= book.nextLine();
+		String authorsName= take.nextLine();
 		
 		System.out.print("Author's surname: ");
-		String authorsSurname= book.nextLine();
+		String authorsSurname= take.nextLine();
 		
 		System.out.print("Publishing house: ");
-		String publishingHouse= book.nextLine();
+		String publishingHouse= take.nextLine();
 		
 		System.out.print("Literary genre: ");
-		String literaryGenre= book.nextLine();
+		String literaryGenre= take.nextLine();
 		
 		System.out.print("Date of publication: ");
-		String dateOfPublication= book.nextLine();
+		int dateOfPublication= take.nextLine();
 		
+		Book book = new Book(title, authorsName, authorsSurname, publishingHouse, literaryGenre, dateOfPublication);
 		
 		System.out.println("\n"+ "Title: "+ title + "\n" + "Author's name: " + authorsName + "\n" + "Author's surname: " + authorsSurname + "\n" + "Publishing house: " + publishingHouse + "\n" + "Literary genre: " + literaryGenre + "\n"+ "Date of publication: " + dateOfPublication);
 			
